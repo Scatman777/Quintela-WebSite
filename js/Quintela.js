b6dc5bd2c5be3e539a70a0a7a122e259e1faa45f
hostr777 <divakovbogdan11@gmail.com>
2023-09-13 19:02:16 +0300

    Update

--- js/Quintela.js
+++ js/Quintela.js
@@ -17,3 +17,24 @@ let elements = document.querySelectorAll('.element-animation');
 for (let elm of elements) {
   observer.observe(elm);
 }
+
+var swiper = new Swiper(".main-swiper", {
+	spaceBetween: 10,
+	navigation: {
+	  nextEl: ".swiper-button-next",
+	  prevEl: ".swiper-button-prev",
+	},
+	watchOverflow: true,
+	slidesPerView: 'auto',
+	slidesPerGroup: 1,
+	slidesPerColumn: 1,
+	dots: false,
+	mousewheel: true,
+	keyboard: true,
+	breakpoints: {
+		630: {
+			spaceBetween: 20,
+			mousewheel: false
+		}
+	}
+});
